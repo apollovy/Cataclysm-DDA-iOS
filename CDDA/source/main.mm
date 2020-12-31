e89411c2a94887ecf01fdb9f53b57c1891b8344f
Yuri Apollov <apollovy@gmail.com>
2020-12-31 03:20:25 +0300

    Update stable to be free of modified CDDA code.

--- CDDA/source/main.mm
+++ CDDA/source/main.mm
@@ -9,11 +9,19 @@ int main( int argc, char *argv[] )
 {
     NSString* documentPath = [[[[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject] path] stringByAppendingString:@"/save/"];
     NSString* datadir = [[[NSBundle mainBundle] bundlePath] stringByAppendingString:@"/data/"];
-    const char* new_argv[] = {
+    const char* new_argv_const[] = {
         *argv,
-        "--datadir", [datadir cStringUsingEncoding:NSUTF8StringEncoding],
-        "--savedir", [documentPath cStringUsingEncoding:NSUTF8StringEncoding],
+        "--datadir", [datadir UTF8String],
+        "--savedir", [documentPath UTF8String],
     };
-
-    return CDDA_main(5, new_argv);
+    const int new_argv_const_size = (sizeof(new_argv_const) / sizeof(*new_argv_const));
+    char* new_argv[new_argv_const_size];
+    
+    for (int i=0; i<(new_argv_const_size); i++) {
+        auto arg_ptr = new_argv_const[i];
+        auto new_ptr = new char[strlen(arg_ptr)];
+        new_argv[i] = strcpy(new_ptr, arg_ptr);
+    };
+    
+    return CDDA_main(new_argv_const_size, new_argv);
 }
