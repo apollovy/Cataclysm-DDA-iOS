f9fc224de79676bc8a0d7e223dcda5c6b9ef5726
Yuri Apollov <apollovy@icloud.com>
2022-12-23 04:17:49 +0700

    Add images to Paywall.storyboard

---
+++ Distinct/Paywall/Paywall debug/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  sadfghj
+//
+//  Created by Аполлов Юрий Андреевич on 23.12.2022.
+//  Copyright © 2022 Аполлов Юрий Андреевич. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
