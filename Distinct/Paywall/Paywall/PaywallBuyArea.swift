a2716d0eb04aab1a47bf0dc5b5cd5f4ee22f7f3e
Yuri Apollov <apollovy@icloud.com>
2022-12-25 04:51:57 +0700

    WIP Moved PaywallBuyArea to xib and MADE IT LOOK ON A STORYBOARD!!!

---
+++ Distinct/Paywall/Paywall/PaywallBuyArea.swift
@@ -0,0 +1,10 @@
+//
+// Created by Аполлов Юрий Андреевич on 25.12.2022.
+// Copyright (c) 2022 Аполлов Юрий Андреевич. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class PaywallBuyAreaWrapper : NibWrapperView<PaywallBuyArea> { }
+
+class PaywallBuyArea: UIView { }
