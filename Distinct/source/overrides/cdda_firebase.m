61ad3a47305477d80a44c23f888bd66777f10ae9
Yuri Apollov <apollovy@icloud.com>
2023-01-13 18:05:37 +0700

    Move analytics to submodule in Libraries.

--- Distinct/source/overrides/cdda_firebase.m
+++ Distinct/source/overrides/cdda_firebase.m
@@ -1,15 +1,9 @@
 //
-//  firebase.h
+//  firebase.m
 //  CDDA
 //
 //  Created by Аполлов Юрий Андреевич on 09.12.2022.
 //  Copyright © 2022 Аполлов Юрий Андреевич. All rights reserved.
 //
 
-#ifndef cdda_firebase_h
-#define cdda_firebase_h
-
-void configureFirebase();
-NSString* getTestGroup();
-
-#endif /* cdda_firebase_h */
+void configureFirebase() {}
