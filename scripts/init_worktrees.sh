a3903ffdc7980e166d1a2137c2a9c0950b4d3aa4
Yuri Apollov <apollovy@icloud.com>
2023-01-14 21:51:32 +0700

    Prepare to get rid of branches for different versions in favor to different targets.

---
+++ scripts/init_worktrees.sh
@@ -0,0 +1,7 @@
+cd submodule/Cataclysm-DDA
+git worktree add ../../worktree/Cataclysm-DDA/0.E 0.E-branch
+git worktree add ../../worktree/Cataclysm-DDA/0.F 0.F-branch
+cd ../Cataclysm-BN
+git worktree add ../../worktree/Cataclysm-BN
+
+cd ../..
